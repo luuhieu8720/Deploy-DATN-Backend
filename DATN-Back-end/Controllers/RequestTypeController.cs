7c2cfd1625a463fca6ba3dfd62fc25503d79927c
luuhieu8720 <lethiluuhieu@gmail.com>
2022-07-17 23:31:37 +0700

    Add apis

---
+++ DATN-Back-end/Controllers/RequestTypeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DATN_Back_end.Dto.DtoDepartment;
+using DATN_Back_end.Dto.DtoRequestType;
+using DATN_Back_end.Models;
+using DATN_Back_end.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DATN_Back_end.Controllers
+{
+    [Route("api/requesttype")]
+    public class RequestTypeController : ControllerBase
+    {
+
+        private IRequestTypeRepository requestTypeRepository;
+
+        public RequestTypeController(IRequestTypeRepository requestTypeRepository)
+        {
+            this.requestTypeRepository = requestTypeRepository;
+        }
+
+        [HttpGet]
+        public async Task<List<RequestTypeItem>> Get() => await requestTypeRepository.Get();
+
+        [HttpPost]
+        public async Task Create([FromBody] RequestTypeForm departmentForm) => await requestTypeRepository.Create(departmentForm);
+
+        [HttpPut("{id}")]
+        public async Task Update(int id, [FromBody] RequestTypeForm departmentForm) => await requestTypeRepository.Update(id, departmentForm);
+    }
+}
